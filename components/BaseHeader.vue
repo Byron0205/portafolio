f7f5486ee530b736965a4e8dc7e2674a2537f926
Byron0205 <gabossosa@gmail.com>
2023-09-27 21:43:48 -0600

    ajuste hover enlaces header

--- components/BaseHeader.vue
+++ components/BaseHeader.vue
@@ -4,22 +4,23 @@
 
             <ul class="mt-6 space-y-1">
                 <li class="">
-                    <NuxtLink to="/" active-class="bg-gray-100 text-gray-700"
-                        class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500">
+                    <NuxtLink to="/" active-class="bg-blue-400 text-white"
+                        class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500
+                        hover:bg-blue-400 hover:text-white">
                         Inicio
                     </NuxtLink>
                 </li>
 
                 <li>
-                    <NuxtLink to="/recursos" active-class="bg-gray-100 text-gray-700"
-                        class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                    <NuxtLink to="/recursos" active-class="bg-blue-400 text-white"
+                        class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-blue-400 hover:text-white">
                         Recursos
                     </NuxtLink>
                 </li>
 
                 <li>
-                    <NuxtLink href="/acercade" active-class="bg-gray-100 text-gray-700"
-                        class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                    <NuxtLink href="/acercade" active-class="bg-blue-400 text-white"
+                        class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-blue-400 hover:text-white">
                         Acerca de
                     </NuxtLink>
                 </li>
