2b9088cdfc5fe4a6513338d1f4cc2727d40e0483
Byron0205 <gabossosa@gmail.com>
2023-09-27 21:40:51 -0600

    cambios tarjeta presentacion estilos

--- components/TarjetaPresentacion.vue
+++ components/TarjetaPresentacion.vue
@@ -1,19 +1,34 @@
 <template>
-    <div class="card-aboutme border-[1px] rounded-[12px] my-6">
-        <div class="container-photo">
-            <img class="photo" src="/img/perfil.jpg" alt="profile photo">
+    <div class="w-fit flex-col flex md:flex-row gap-4 justify-center items-center p-4 border-[1px] rounded-[12px] my-6">
+        <div class="w-[200px] md:w-[260px]">
+            <img class="rounded-[15px] w-fit" src="/img/perfil.jpg" alt="profile photo">
         </div>
-        <div class="text">
-            <div class="title">
-                <p class="name">Byron Sosa Hidalgo</p>
-                <p class="grade">Desarrollador junior</p>
+        <div class="flex flex-col">
+            <div>
+                <p class="text-2xl text-center md:text-left md:text-3xl font-semibold">Byron Sosa Hidalgo</p>
+                <p class="font-bold text-center md:text-left text-gray-500">Desarrollador junior</p>
             </div>
-            <div class="contact">
-                <p>gabossosa@gmail.com</p>
-                <p>(+506) 8623 2916</p>
-            </div>
-            <div class="description">
+            <div class="my-2 text-gray-600 text-center md:text-left">
                 Desarrollador creativo, colaborador y activo a la hora de trabajar en equipo.
+            </div>
+            <div class="flex flex-wrap justify-center gap-2 my-4">
+                <div class="p-2 bg-blue-400 rounded-[12px]">
+                    <Icon name="uil:facebook" color="white" size="30px" />
+                </div>
+                <div class="p-2 bg-blue-400 rounded-[12px]">
+                    <Icon name="uil:instagram" color="white" size="30px" />
+                </div>
+                <div class="p-2 bg-blue-400 rounded-[12px]">
+                    <Icon name="uil:youtube" color="white" size="30px" />
+                </div>
+                <div class="p-2 bg-blue-400 rounded-[12px]">
+                    <Icon name="uil:linkedin" color="white" size="30px" />
+                </div>
+                <div class="p-2 bg-blue-400 rounded-[12px]">
+                    <a href="mailto:gabossosa@gmail.com">
+                        <Icon name="tabler:mail" color="white" size="30px" />
+                    </a>
+                </div>
             </div>
         </div>
     </div>
@@ -25,6 +40,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss','@nuxt/content'],
+  modules: ['@nuxtjs/tailwindcss','@nuxt/content', 'nuxt-icon'],
   css: ['~/assets/base.css']
 })
