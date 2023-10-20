d3e83cebe8c8f8aeb9c25a91c259eace8bfa78c1
Byron0205 <gabossosa@gmail.com>
2023-10-19 19:58:30 -0600

    fix(hobbys): ajuste visual de pasatiempos

--- components/Habilidades.vue
+++ components/Habilidades.vue
@@ -2,35 +2,35 @@
     <Container class="w-full">
         <TitleCard>Habilidades</TitleCard>
         <div class="flex flex-wrap justify-center items-center gap-4 my-3">
-                <p class="text-[20px] text-center" title="python">
+                <p class="text-[20px] text-center cursor-pointer" title="python">
                 <Icon name="skill-icons:python-light" size="50px"/>
                 </p>
             
-                <p class="text-[20px] text-center" title="javascript">
+                <p class="text-[20px] text-center cursor-pointer" title="javascript">
                 <Icon name="skill-icons:javascript" size="50px"/>        
                 </p>
 
-                <p class="text-[20px] text-center" title="vue">
+                <p class="text-[20px] text-center cursor-pointer" title="vue">
                 <Icon name="skill-icons:vuejs-light" size="50px"/>
                 </p>
                 
-                <p class="text-[20px] text-center" title="css">
+                <p class="text-[20px] text-center cursor-pointer" title="css">
                 <Icon name="skill-icons:css" size="50px"/>
                 </p>
                 
-                <p class="text-[20px] text-center" title="fastapi">
+                <p class="text-[20px] text-center cursor-pointer" title="fastapi">
                 <Icon name="skill-icons:fastapi" size="50px"/>
                 </p>
                 
-                <p class="text-[20px] text-center" title="MySql">
+                <p class="text-[20px] text-center cursor-pointer" title="MySql">
                 <Icon name="skill-icons:mysql-light" size="50px"/>
                 </p>
                 
-                <p class="text-[20px] text-center" title="SqlServer">
+                <p class="text-[20px] text-center cursor-pointer" title="SqlServer">
                 <Icon name="devicon-plain:microsoftsqlserver-wordmark" size="50px"/>
                 </p>
                 
-                <p class="text-[20px] text-center" title="Git">
+                <p class="text-[20px] text-center cursor-pointer" title="Git">
                 <Icon name="skill-icons:git" size="50px"/>
                 </p>
 
--- components/hobby.vue
+++ components/hobby.vue
@@ -1,23 +1,30 @@
 <template>
-    <Container class="w-full">
+    <Container>
         <h2 class="text-3xl text-center font-semibold">Pasatiempos</h2>
-        <div class="grid p-5 justify-center my-auto h-full items-center">
-                <p class="text-2xl flex gap-4">
-                    <Icon name="fluent-emoji:basketball" size="30px" /> <span>Basketball</span> 
-                </p>
-                <p class="text-2xl flex gap-4">
-                    <Icon name="twemoji:soccer-ball" size="30px" /> <span>Futbol</span>
-                </p>
-
-                <p class="text-2xl flex gap-4">
-                    <Icon name="noto:open-book" size="30px" /><span>Leer</span>
-                </p>
-                <p class="text-2xl flex gap-4">
-                    <Icon name="dashicons:games" color="black" size="30px" /><span>Videojuegos</span>
-                </p>
-                <p class="text-2xl flex gap-4">
-                    <Icon name="gg:gym" color="black" size="30px" /><span>Gym</span>
-                </p>
+        <div class="flex flex-wrap gap-5 p-5 justify-center mt-4 items-center">
+            <p class="text-2xl bg-sky-300 p-2 rounded-[12px] cursor-pointer" title="Futbol">
+                <Icon name="twemoji:soccer-ball" size="40px" /> <!-- <span>Futbol</span> -->
+            </p>
+            
+            <p class="text-2xl bg-yellow-300 p-2 rounded-[12px] cursor-pointer" title="Pesas">
+                <Icon name="gg:gym" color="black" size="40px" /><!-- <span>Gym</span> -->
+            </p>
+            
+            <p class="text-2xl bg-green-300 p-2 rounded-[12px] cursor-pointer" title="Leer">
+                <Icon name="noto:open-book" size="40px" /><!-- <span>Leer</span> -->
+            </p>
+            
+            <p class="text-2xl bg-blue-300 p-2 rounded-[12px] cursor-pointer" title="Basketball">
+                <Icon name="fluent-emoji:basketball" size="40px" /> <!-- <span>Basketball</span> --> 
+            </p>
+            
+            <p class="text-2xl bg-orange-300 p-2 rounded-[12px] cursor-pointer" title="Jugar Videojuegos">
+                <Icon name="dashicons:games" color="black" size="40px" /><!-- <span>Videojuegos</span> -->
+            </p>
+            
+            <p class="text-2xl bg-lime-300 p-2 rounded-[12px] cursor-pointer" title="Paseo en bicicleta">
+                <Icon name="healthicons:bike-outline" color="black" size="40px" /><!-- <span>Bicicleta</span> -->
+            </p>
 
         </div>
     </Container>
